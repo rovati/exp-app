bbaec46bcbf245fb4ed3e9892682b09adec8e1fc
rovati <lorenzo.rovati@tutanota.com>
2022-01-03 20:52:49 +0100

    Add informative messages

--- exp_app/lib/widget/expense_list_body.dart
+++ exp_app/lib/widget/expense_list_body.dart
@@ -1,6 +1,10 @@
 import 'package:exp/model/expense_entry.dart';
 import 'package:exp/model/expense_list.dart';
+import 'package:exp/util/constant/animations.dart';
+import 'package:exp/util/constant/strings.dart';
+import 'package:exp/util/constant/text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'expense_tile.dart';
 
@@ -11,25 +15,41 @@ class ExpenseListBody extends StatelessWidget {
   const ExpenseListBody(this.expenseList, {Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => ListView.builder(
-        physics: const BouncingScrollPhysics(),
-        itemCount: expenseList.allEntries.length,
-        itemBuilder: (context, index) {
-          ExpenseEntry entry = expenseList.allEntries[index];
-          final paddingFst = EdgeInsets.only(
-            top: MediaQuery.of(context).size.height * 0.02,
-            bottom: MediaQuery.of(context).size.height * 0.005,
-            left: MediaQuery.of(context).size.width * 0.05,
-            right: MediaQuery.of(context).size.width * 0.05,
-          );
-          final padding = EdgeInsets.symmetric(
-            vertical: MediaQuery.of(context).size.height * 0.005,
-            horizontal: MediaQuery.of(context).size.width * 0.05,
-          );
-          return Padding(
-            padding: index == 0 ? paddingFst : padding,
-            child: ExpenseTile(entry),
-          );
-        },
+  Widget build(BuildContext context) => Consumer<ExpenseList>(
+        builder: (context, expenselist, child) => AnimatedSwitcher(
+          duration: Animations.animDur,
+          child: expenselist.entries.isEmpty
+              ? Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: Center(
+                    child: Text(
+                      Strings.emptyExpBody,
+                      style: TextStyles.lBlue20,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                )
+              : ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: expenseList.allEntries.length,
+                  itemBuilder: (context, index) {
+                    ExpenseEntry entry = expenseList.allEntries[index];
+                    final paddingFst = EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.02,
+                      bottom: MediaQuery.of(context).size.height * 0.005,
+                      left: MediaQuery.of(context).size.width * 0.05,
+                      right: MediaQuery.of(context).size.width * 0.05,
+                    );
+                    final padding = EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.height * 0.005,
+                      horizontal: MediaQuery.of(context).size.width * 0.05,
+                    );
+                    return Padding(
+                      padding: index == 0 ? paddingFst : padding,
+                      child: ExpenseTile(entry),
+                    );
+                  },
+                ),
+        ),
       );
 }
--- exp_app/lib/widget/home_list_body.dart
+++ exp_app/lib/widget/home_list_body.dart
@@ -1,5 +1,8 @@
 import 'package:exp/model/home_list.dart';
 import 'package:exp/model/list_info.dart';
+import 'package:exp/util/constant/animations.dart';
+import 'package:exp/util/constant/strings.dart';
+import 'package:exp/util/constant/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,25 +13,38 @@ class HomeListBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Consumer<HomeList>(
-      builder: (context, homelist, child) => ListView.builder(
-            physics: const BouncingScrollPhysics(),
-            itemCount: homelist.lists.length,
-            itemBuilder: (context, index) {
-              ListInfo info = homelist.lists[index];
-              final paddingFst = EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.02,
-                bottom: MediaQuery.of(context).size.height * 0.005,
-                left: MediaQuery.of(context).size.width * 0.05,
-                right: MediaQuery.of(context).size.width * 0.05,
-              );
-              final padding = EdgeInsets.symmetric(
-                vertical: MediaQuery.of(context).size.height * 0.005,
-                horizontal: MediaQuery.of(context).size.width * 0.05,
-              );
-              return Padding(
-                padding: index == 0 ? paddingFst : padding,
-                child: ListInfoTile(info),
-              );
-            },
-          ));
+      builder: (context, homelist, child) => AnimatedSwitcher(
+          duration: Animations.animDur,
+          child: homelist.lists.isEmpty
+              ? Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: Center(
+                    child: Text(
+                      Strings.emptyHomeBody,
+                      style: TextStyles.lBlue20,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                )
+              : ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: homelist.lists.length,
+                  itemBuilder: (context, index) {
+                    ListInfo info = homelist.lists[index];
+                    final paddingFst = EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.02,
+                      bottom: MediaQuery.of(context).size.height * 0.005,
+                      left: MediaQuery.of(context).size.width * 0.05,
+                      right: MediaQuery.of(context).size.width * 0.05,
+                    );
+                    final padding = EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.height * 0.005,
+                      horizontal: MediaQuery.of(context).size.width * 0.05,
+                    );
+                    return Padding(
+                      padding: index == 0 ? paddingFst : padding,
+                      child: ListInfoTile(info),
+                    );
+                  },
+                )));
 }
