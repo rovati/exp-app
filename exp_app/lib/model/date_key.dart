862405c8c1fd7b5eb1aceea1a547946045b96292
rovati <lorenzo.rovati@tutanota.com>
2022-01-04 11:56:29 +0100

    Implementation of expense summary model

--- exp_app/lib/model/date_key.dart
+++ exp_app/lib/model/date_key.dart
@@ -7,6 +7,45 @@ class DateKey {
 
   DateKey(this.year, this.month);
 
+  /// Returns a date key for the month after.
+  DateKey next() {
+    int nextYear = year;
+    int nextMonth = month;
+    if (month == 12) {
+      nextYear += 1;
+      nextMonth = 1;
+    } else {
+      nextMonth += 1;
+    }
+    return DateKey(nextYear, nextMonth);
+  }
+
+  /// Returns a date key for the previous month.
+  DateKey prev() {
+    int prevYear = year;
+    int prevMonth = month;
+    if (month == 1) {
+      prevYear -= 1;
+      prevMonth = 12;
+    } else {
+      prevMonth -= 1;
+    }
+    return DateKey(prevYear, prevMonth);
+  }
+
+  /// Returns true if this date is before the given date.
+  /// NOTE returns false if the dates are the same.
+  bool isPriorTo(DateKey other) {
+    if (year < other.year) {
+      return true;
+    } else {
+      if (year == other.year && month < other.month) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   /// Equality is defined as having same year and same month.
   @override
   bool operator ==(Object other) {
--- exp_app/lib/model/expense_summary.dart
+++ exp_app/lib/model/expense_summary.dart
@@ -1,6 +1,9 @@
 import 'package:exp/model/date_key.dart';
+import 'package:flutter/material.dart';
 
-class ExpenseSummary {
+import 'expense_entry.dart';
+
+class ExpenseSummary extends ChangeNotifier {
   static final ExpenseSummary _summary = ExpenseSummary._internal();
 
   // DateKey is mapped to a map from list id to the amount of that list.
@@ -14,13 +17,26 @@ class ExpenseSummary {
         _oldestDate = DateKey(DateTime.now().year, DateTime.now().month);
 
   // TODO
-  void addEntry(int listID, double amount) {
+  void init() {
+    _amounts.clear();
+    _oldestDate = DateKey(DateTime.now().year, DateTime.now().month);
     throw UnimplementedError();
+    // receive json {id -> {expenselist data}} from dbhelper
+    // generate map {id -> list[expenseentry]}
+    // silentadd all entries
   }
 
-  // TODO
-  void removeEntry(int listID, double amount) {
-    throw UnimplementedError();
+  void addEntry(int listID, ExpenseEntry entry) {
+    _silentAdd(listID, entry);
+    notifyListeners();
+  }
+
+  void removeEntry(int listID, ExpenseEntry entry) {
+    final key = DateKey(entry.date.year, entry.date.month);
+    final amount = entry.amount;
+    if (_amounts.containsKey(key) && _amounts[key]!.containsKey(listID)) {
+      _amounts[key]![listID] = _amounts[key]![listID]! - amount;
+    }
   }
 
   // TODO
@@ -28,21 +44,36 @@ class ExpenseSummary {
     throw UnimplementedError();
   }
 
-  // TODO
   /// Returns a list of each month total expense with one entry each month from
-  /// the oldest date with an expense up to the current month.
+  /// the current month down to oldest date with an expense.
   /// NOTE fills with 0 where there was no registered expenses.
   List<double> amount() {
-    throw UnimplementedError();
+    DateKey currentDateKey = DateKey(DateTime.now().year, DateTime.now().month);
+    List<double> res = [];
+    while (_oldestDate.isPriorTo(currentDateKey)) {
+      res.add(_monthAmount(currentDateKey));
+      currentDateKey = currentDateKey.prev();
+    }
+    return res;
   }
 
-  // TODO
   /// Returns a list of each month expense of the given list with one entry each
   /// month from the oldest date with an expense up to the current month.
   /// NOTE fills with 0 where there was no registered expenses for the given
   /// list.
   List<double> listAmount(int listID) {
-    throw UnimplementedError();
+    DateKey currentDateKey = DateKey(DateTime.now().year, DateTime.now().month);
+    List<double> res = [];
+    while (_oldestDate.isPriorTo(currentDateKey)) {
+      if (!_amounts.containsKey(currentDateKey) ||
+          !_amounts[currentDateKey]!.containsKey(listID)) {
+        res.add(0);
+      } else {
+        res.add(_amounts[currentDateKey]![listID]!);
+      }
+      currentDateKey = currentDateKey.prev();
+    }
+    return res;
   }
 
   // TODO
@@ -55,5 +86,31 @@ class ExpenseSummary {
   /// Returns the expense of the given list for the given year-month.
   double listAmountFor(int listID, DateKey key) {
     throw UnimplementedError();
+  }
+
+  void _silentAdd(int listID, ExpenseEntry entry) {
+    final key = DateKey(entry.date.year, entry.date.month);
+    final amount = entry.amount;
+    if (!_amounts.containsKey(key)) {
+      _amounts[key] = {};
+    }
+    if (!_amounts[key]!.containsKey(listID)) {
+      _amounts[key]![listID] = 0;
+    }
+    _amounts[key]![listID] = _amounts[key]![listID]! + amount;
+    _oldestDate = key.isPriorTo(_oldestDate) ? key : _oldestDate;
+  }
+
+  /// Accumulates the amounts of the various lits for a given date key.
+  double _monthAmount(DateKey key) {
+    double monthTot = 0;
+    if (!_amounts.containsKey(key)) {
+      return 0;
+    } else {
+      for (int id in _amounts[key]!.keys) {
+        monthTot += _amounts[key]![id]!;
+      }
+      return monthTot;
+    }
   }
 }
